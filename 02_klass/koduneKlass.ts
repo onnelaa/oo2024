536595cb54dd185954faa9ea83017c3ee39861ce
Õnnela Link <146323564+onnelaa@users.noreply.github.com>
2024-02-22 12:34:46 +0200

    Update koduneKlass.ts

--- 02_klass/koduneKlass.ts
+++ 02_klass/koduneKlass.ts
@@ -11,16 +11,16 @@ class Car {
 
     calculateFuelCost(distance: number, fuelPrice: number) {
         const fuelCost = (distance / 100) * this.fuelConsumption * fuelPrice;
-        console.log(this.brand + ' ' + this.model + ' küttekulu, vahemikus: ' + distance + ' km on ' + fuelCost.toFixed(2) + ' €');
+        console.log(this.brand + ' ' + this.model + ' küttekulu hind, vahemikus: ' + distance + ' km on ' + fuelCost.toFixed(2) + ' €');
     }
 }
 
 //Esimene auto
 let car1 = new Car("Toyota", "Corolla", 6.5);
 car1.calculateFuelCost(300, 1.5); 
-//Väljund 1: Toyota Corolla küttekulu, vahemikus: 300 km on 29.25 €
+//Väljund 1: Toyota Corolla küttekulu hind, vahemikus: 300 km on 29.25 €
 
 //Teine auto
 let car2 = new Car("Ford", "Mustang", 12);
 car2.calculateFuelCost(300, 1.7); 
-//Väljund 2: Ford Mustang küttekulu, vahemikus: 300 km on 61.20 €
+//Väljund 2: Ford Mustang küttekulu hind, vahemikus: 300 km on 61.20 €
