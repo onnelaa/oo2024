c28bb09c42f94b837f3fc5b91a480b1c8abaa60a
Õnnela Link <146323564+onnelaa@users.noreply.github.com>
2024-04-11 15:19:59 +0300

    Update liides.ts

--- 06_liides/liides.ts
+++ 06_liides/liides.ts
@@ -8,16 +8,16 @@ class AverageFuelConsumption {
 }
 
 document.getElementById("fuelForm")!.addEventListener("submit", function(event) {
-  event.preventDefault();
+  event.preventDefault(); //takistab vaikimisi vormi esitamise käitumist
   
-  const distanceInput = document.getElementById("distance") as HTMLInputElement;
-  const fuelAmountInput = document.getElementById("fuelAmount") as HTMLInputElement;
+  const distanceInput = document.getElementById("distance") as HTMLInputElement; 
+  const fuelAmountInput = document.getElementById("fuelAmount") as HTMLInputElement; //võtavad kinni kauguse ja kütuse sisestuse väärtused vastavalt nende elementide id-dele.
   
   const distance = parseFloat(distanceInput.value);
   const fuelAmount = parseFloat(fuelAmountInput.value);
   
-  const calculator = new AverageFuelConsumption();
-  const averageConsumption = calculator.calculate(distance, fuelAmount);
+  const calculator = new AverageFuelConsumption(); //loob uue objekti 
+  const averageConsumption = calculator.calculate(distance, fuelAmount); //kasutab loodud objekti, et arvutada keskmine kütusekulu, kasutades sisestatud kaugust ja kütusekogust.
   
   const resultElement = document.getElementById("result");
   if (resultElement) {
