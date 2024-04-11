7f36e5b1bca76a87410f700adfc5cadc0cb5bfa0
Õnnela Link <146323564+onnelaa@users.noreply.github.com>
2024-04-11 15:44:06 +0300

    Update koduTest.ts

--- 07_testid/koduTest.ts
+++ 07_testid/koduTest.ts
@@ -7,17 +7,17 @@ beforeEach(() => {
 });
 
 test('algne laoseis', () => {
-  expect(ladu!.saaLaoseis()).toBe(5); //kontrolli, kas algne laoseis on õige
+  expect(ladu.saaLaoseis()).toBe(5); //kontrolli, kas algne laoseis on õige
 });
 
 test('osta yks laud', () => {
-  ladu!.ostaLaud(); //osta üks laud
-  expect(ladu!.saaLaoseis()).toBe(4); //kontrolli, kas laoseis väheneb pärast ühe laua ostu
+  ladu.ostaLaud(); //osta üks laud
+  expect(ladu.saaLaoseis()).toBe(4); //kontrolli, kas laoseis väheneb pärast ühe laua ostu
 });
 
 test('osta koik lauad', () => {
   for (let i = 0; i < 5; i++) {
-    ladu!.ostaLaud(); //osta kõik lauad
+    ladu.ostaLaud(); //osta kõik lauad
   }
-  expect(ladu!.saaLaoseis()).toBe(0); //kontrolli, kas laoseis on null pärast kõikide laudade ostu
+  expect(ladu.saaLaoseis()).toBe(0); //kontrolli, kas laoseis on null pärast kõikide laudade ostu
 });
